8c5a2f8ab09fd0cb68b8d0debb3f17f7bb47e6a4
maki <maki@maki-Aspire-S3-391.(none)>
2013-07-14 15:32:31 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mi::Application.config.secret_key_base = '7de298cdaf270c18d6ec8a3d4b1b8408c46a7032b6470a2fd153ad4d9ddbf1f4618636360f23537dcc22a3bfe9c0a4f02a217e5042a878488e25a92d0b10fb75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mi::Application.config.session_store :cookie_store, key: '_mi_session'
